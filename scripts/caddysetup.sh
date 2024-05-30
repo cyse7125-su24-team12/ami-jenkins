f50b95dc83a80ff2f684b74f4f194331278b7fff
Shyam Sundar Mylai <mylai.s@northeastern.edu>
2024-05-30 18:36:29 -0400

    caddy setup changes

--- scripts/caddysetup.sh
+++ scripts/caddysetup.sh
@@ -12,17 +12,8 @@ curl -1sLf 'https://dl.cloudsmith.io/public/caddy/testing/debian.deb.txt' | sudo
 sudo apt-get update
 sudo apt-get install -y caddy
 
-# Create the certificates directory and set permissions (fix for error)
-sudo mkdir -p /var/lib/caddy/.local/share/caddy/pki/authorities/local
-sudo chown -R caddy:caddy /var/lib/caddy/.local/share/caddy
-sudo chmod -R 0755 /var/lib/caddy/.local/share/caddy
-
 # Define the Caddyfile content (improved)
 CADDYFILE_CONTENT="
-{
-        acme_ca https://acme-staging-v02.api.letsencrypt.org/directory
-}
- 
 jenkins.cloudnativewebapp.me {
         reverse_proxy :8080
 }
@@ -34,10 +25,11 @@ echo "$CADDYFILE_CONTENT" | sudo tee /etc/caddy/Caddyfile
 # Format the Caddyfile for better readability (optional)
 sudo caddy fmt --overwrite /etc/caddy/Caddyfile
 
+# Restart the caddy service to pickup the new config
 sudo service caddy restart
 
+# Fireball settings - Allow port 443 
 sudo ufw allow 443
-
 sudo ufw reload
 
 echo "Caddy installed & proxy updated successfully"
