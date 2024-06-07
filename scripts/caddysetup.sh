dc0a8415374ae227c58a04369d3570d315b032f0
Balasubramanian Ulaganathan <75071699+BalasubramanianU@users.noreply.github.com>
2024-06-07 00:30:46 -0400

    Update caddysetup.sh

--- scripts/caddysetup.sh
+++ scripts/caddysetup.sh
@@ -14,11 +14,7 @@ sudo apt-get install -y caddy
 
 # Define the Caddyfile content (improved)
 CADDYFILE_CONTENT="
-{
-        acme_ca https://acme-staging-v02.api.letsencrypt.org/directory
-}
-
-jenkinsv2.cloudnativewebapp.me {
+jenkins.cloudnativewebapp.me {
         reverse_proxy :8080
 }
 "
