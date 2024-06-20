b5eced9c07cb3ba5d32f6d06510c716d2f933629
Shyam Sundar Mylai <mylai.s@northeastern.edu>
2024-06-20 17:14:05 -0400

    chore: readme udapted

--- scripts/setupjenkins.sh
+++ scripts/setupjenkins.sh
@@ -321,7 +321,7 @@ multibranchPipelineJob('infra-aws-job') {
       id('csye7125-su24-t12-infra-aws')
       scanCredentialsId('git-credentials-id')
       repoOwner('cyse7125-su24-team12')
-      repository('ami-jenkins')
+      repository('infra-aws')
       buildForkPRMerge(true)
       buildOriginBranch(false)
       buildOriginBranchWithPR(false)
